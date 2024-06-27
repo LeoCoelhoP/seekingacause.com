eb3462b54737418f5c3a3ada34d4250ba6fd1854
LeoCoelhoP <148665981+LeoCoelhoP@users.noreply.github.com>
2024-06-27 00:11:48 -0300

    Fix: Axios config

--- src/Configs/axios.js
+++ src/Configs/axios.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_BACK_END_URL,
-	withCredentials: true,
-	headers: {
-		'Content-Type': 'application/json;charset=UTF-8;multipart/form-data',
-	},
+	// withCredentials: true,
+	// headers: {
+	// 	'Content-Type': 'application/json;charset=UTF-8;multipart/form-data',
+	// },
 });
 
 axiosInstance.interceptors.response.use(
@@ -16,10 +16,10 @@ axiosInstance.interceptors.response.use(
 		),
 );
 
-axiosInstance.AxiosHeaders = {
-	'Access-Control-Allow-Origin': import.meta.env.BACK_END_URL,
-	'Access-Control-Allow-Headers': 'Authorization',
-	'Access-Control-Allow-Methods': 'GET, POST, PATCH',
-};
+// axiosInstance.AxiosHeaders = {
+// 	'Access-Control-Allow-Origin': import.meta.env.BACK_END_URL,
+// 	'Access-Control-Allow-Headers': 'Authorization',
+// 	'Access-Control-Allow-Methods': 'GET, POST, PATCH',
+// };
 
 export default axiosInstance;
