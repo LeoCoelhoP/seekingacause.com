5cbf437629d28d0055786bd2bb8d068aef83d7a0
LeoCoelhoP <148665981+LeoCoelhoP@users.noreply.github.com>
2024-06-28 15:32:02 -0300

    Fix: Ads file problem

--- src/Components/Ads.jsx
+++ src/Components/Ads.jsx
@@ -3,9 +3,10 @@ import { Adsense } from '@ctrl/react-adsense';
 export default function Ads() {
 	return (
 		<Adsense
-  client="ca-pub-7640562161899788"
-  slot="7259870550"
-  style={{ width: '100%', height: '100px' }}
-  format=""
-/>
+			client='ca-pub-7640562161899788'
+			slot='7259870550'
+			style={{ width: '100%', height: '100px' }}
+			format=''
+		/>
+	);
 }
