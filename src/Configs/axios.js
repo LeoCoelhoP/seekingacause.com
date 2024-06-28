ebb8301ddb9d4d7b444c2ead08ed09a228b30c22
LeoCoelhoP <148665981+LeoCoelhoP@users.noreply.github.com>
2024-06-27 22:27:36 -0300

    Remove with credentials from axios config

--- src/Configs/axios.js
+++ src/Configs/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_BACK_END_URL,
-	withCredentials: true,
+	// withCredentials: true,
 });
 
 axiosInstance.interceptors.response.use(
