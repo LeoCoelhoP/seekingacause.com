0244120653874aae7e745cf60a47cac8c0252a63
Leonardo <148665981+LeoCoelhoP@users.noreply.github.com>
2024-09-11 18:43:34 -0300

    Remove: Remove profile picture due to privacy

--- src/App.jsx
+++ src/App.jsx
@@ -20,49 +20,50 @@ const storedLang = localStorage.getItem('i18nextLng') || 'en';
 i18next.changeLanguage(storedLang);
 
 const initialOptions = {
-	'client-id': import.meta.env.VITE_PAYPAL_ID,
-	currency: 'BRL',
-	intent: 'capture',
+  'client-id': import.meta.env.VITE_PAYPAL_ID,
+  currency: 'BRL',
+  intent: 'capture',
 };
 
 const routes = [
-	{ path: '/', element: <Home />, layoutProps: { showHeader: true } },
-	{
-		path: '/favorites',
-		element: <Favorites />,
-		layoutProps: { showHeader: true },
-	},
-	{ path: '/auth', element: <Auth /> },
-	{ path: '/profile', element: <Profile /> },
-	{ path: '/ngo/:id', element: <Ngo />, layoutProps: { showNav: true } },
-	{ path: '/verify-email', element: <Auth /> },
-	{ path: '/reset-password', element: <Auth resetPassword={true} /> },
+  { path: '/', element: <Home />, layoutProps: { showHeader: true } },
+  {
+    path: '/favorites',
+    element: <Favorites />,
+    layoutProps: { showHeader: true },
+  },
+  { path: '/auth', element: <Auth /> },
+  { path: '/profile', element: <Profile /> },
+  { path: '/ngo/:id', element: <Ngo />, layoutProps: { showNav: true } },
+  { path: '/verify-email', element: <Auth /> },
+  { path: '/reset-password', element: <Auth resetPassword={true} /> },
+  { path: '*', element: <Home />, layoutProps: { showHeader: true } },
 ];
 
 const router = createBrowserRouter(
-	routes.map(({ element, layoutProps, ...route }) => ({
-		...route,
-		element: (
-			<Suspense fallback={<Loading />}>
-				<MainLayout {...layoutProps}>{element}</MainLayout>
-			</Suspense>
-		),
-	})),
+  routes.map(({ element, layoutProps, ...route }) => ({
+    ...route,
+    element: (
+      <Suspense fallback={<Loading />}>
+        <MainLayout {...layoutProps}>{element}</MainLayout>
+      </Suspense>
+    ),
+  }))
 );
 
 function App() {
-	return (
-		<UserProvider>
-			<NgoProvider>
-				<LayoutProvider>
-					<Toaster toastOptions={{ duration: 5000 }} />
-					<PayPalScriptProvider options={initialOptions}>
-						<RouterProvider router={router} />
-					</PayPalScriptProvider>
-				</LayoutProvider>
-			</NgoProvider>
-		</UserProvider>
-	);
+  return (
+    <UserProvider>
+      <NgoProvider>
+        <LayoutProvider>
+          <Toaster toastOptions={{ duration: 5000 }} />
+          <PayPalScriptProvider options={initialOptions}>
+            <RouterProvider router={router} />
+          </PayPalScriptProvider>
+        </LayoutProvider>
+      </NgoProvider>
+    </UserProvider>
+  );
 }
 
 export default App;
